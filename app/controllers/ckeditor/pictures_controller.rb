1ea2fd32b907bb4d269b045307a427e89b3c2501
noxmwalsh <noxiousthing@gmail.com>
2015-04-06 15:18:51 -0700

    Enforced a lower page display limit so our images don't timeout every time.

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,8 +1,10 @@
 class Ckeditor::PicturesController < Ckeditor::ApplicationController
 
+  CKEDITOR_FILEBROWSER_LIMIT = 10
+
   def index
     @pictures = Ckeditor.picture_adapter.find_all(ckeditor_pictures_scope)
-    @pictures = Ckeditor::Paginatable.new(@pictures).page(params[:page])
+    @pictures = Ckeditor::Paginatable.new(@pictures, {:limit => CKEDITOR_FILEBROWSER_LIMIT}).page(params[:page])
 
     respond_to do |format|
       format.html { render :layout => @pictures.first_page? }
